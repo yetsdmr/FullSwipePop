b74e2e3286f3b3ebac91e996a6c69080dc33eb9f
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-09 16:33:43 +0300

    Initial Commit

---
+++ FullSwipePop/FullSwipePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullSwipePopApp.swift
+//  FullSwipePop
+//
+//  Created by Yunus Emre Taşdemir on 9.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FullSwipePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
